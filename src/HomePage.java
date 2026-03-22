2e2328e96f1f1d2a8f991d8c2eb16152dc8383aa
Akiva Cohen <Akivaecohen@gmail.com>
2026-03-22 19:22:59 -0400

    homepage edits

--- src/HomePage.java
+++ src/HomePage.java
@@ -4,11 +4,15 @@ public class HomePage {
         GUI.frame.dispose();
         GUI.createWindow("Home Page");
         GUI.addLabel("Welcome to the Home Page!", 20);
-        GUI.addButton("Chumash", () -> System.out.print("work in progress"));
+        GUI.addButton("Chumash", () -> replace());
         GUI.addButton("Quiz", () -> quiz.run());
         
         TextToSpeech.speak("Login successful.");
         TextToSpeech.speak("Welcome to the home page. Please select an option to continue.");
         TextToSpeech.speak("Option 1: Chumash. Option 2: Quiz.");
+    }
+    public static void replace() {
+        GUI.frame.removeAll();
+        GUI.frame.add(new PassukSelector());
     }
 }
