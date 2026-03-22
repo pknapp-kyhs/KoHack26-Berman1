aa2ac6221fe972a4e4fd6313e9e68ab85a881d10
Akiva Cohen <Akivaecohen@gmail.com>
2026-03-22 13:30:51 -0400

    working parser

--- src/PerekAdapter.java
+++ src/PerekAdapter.java
@@ -1,15 +1,24 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.reflect.Type;
 
 import com.google.gson.*;
 
 public class PerekAdapter implements JsonDeserializer<String[][]> {
     @Override
-    public String[][] deserialize(JsonElement element, Type type, JsonDeserializationContext context) throws JsonParseException {
-        List<List<String>> result = new ArrayList<>();
-        
+    public String[][] deserialize(JsonElement element, Type typeOf, JsonDeserializationContext context) throws JsonParseException {
+
         if (element.isJsonPrimitive()) {
-            result.add(Collections.)
+            String[][] out = {{element.getAsString()}};
+            return out;
+        } else if (element.isJsonArray()) {
+            JsonArray jsonArray = element.getAsJsonArray();
+            if (jsonArray.size() > 0 && jsonArray.get(0).isJsonArray()) {
+                return context.deserialize(jsonArray, new String[0][0].getClass());
+            } else {
+                String[][] out = {context.deserialize(jsonArray, new String[0].getClass())};
+                return out;
+            }
+        } else {
+            throw new JsonParseException("errorrrrrr");
         }
     }
 }
