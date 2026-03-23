ba89ebae139475fe88566510e7c72d164d5b7b1b
Akiva Cohen <Akivaecohen@gmail.com>
2026-03-23 12:34:39 -0400

    upload

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 import javax.swing.UIManager;
 
+import org.w3c.dom.Text;
+
 public class App {   
     public static void main(String[] args) throws Exception {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
