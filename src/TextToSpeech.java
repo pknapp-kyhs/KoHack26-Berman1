c82d2addbf91eccd9c962428bd879b512852a75e
jxds99 <yehuda.dachman@mjbha.org>
2026-03-22 21:52:11 -0400

    bloogey

--- src/TextToSpeech.java
+++ src/TextToSpeech.java
@@ -5,16 +5,21 @@ import java.util.concurrent.Executors;
 
 public class TextToSpeech {
     static ExecutorService executor = Executors.newSingleThreadExecutor();
+
+    static Voice voice;
     static void speak(String textToSpeak){
         System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
         VoiceManager vm;
         vm = VoiceManager.getInstance();
-        Voice voice  = vm.getVoice("kevin");
+        voice = vm.getVoice("kevin");
         executor.execute(() -> {
             voice.allocate();
             voice.speak(textToSpeak);
             voice.deallocate();
         });
        
+    }
+    public static void stopAudio(){
+        voice.getAudioPlayer().cancel();
     }
 }
